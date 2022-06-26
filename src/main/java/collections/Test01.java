9358d737c3d456f0f9efd0a55c485607e108e5c6
shawn <shawn.wu6688@gmail.com>
2022-06-26 19:20:24 +0800

    面试

---
+++ src/main/java/collections/Test01.java
@@ -0,0 +1,27 @@
+package collections;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.*;
+
+/**
+ * @ProjectName: interview_practice
+ * @Package: collections
+ * @ClassName: test01
+ * @Description: []
+ * @Author: [Shawn Wu]
+ * @Date: 2022/6/13 18:39
+ * @Version: V1.0
+ * @Copyright: 2021  Inc. All rights reserved.
+ * @TODO: 注意, 本文件Shawn Wu 所作,如果转载或使用请标明具体出处!
+ **/
+public class Test01 {
+    public static void main(String[] args) {
+        HashMap<Object, Object> map = new HashMap<>();
+        Map<Object, Object> map1 = Collections.synchronizedMap(map);
+        ConcurrentHashMap<Object, Object> cMap = new ConcurrentHashMap<>();
+        //cMap.put()
+        ExecutorService pool = Executors.newCachedThreadPool();
+    }
+}
